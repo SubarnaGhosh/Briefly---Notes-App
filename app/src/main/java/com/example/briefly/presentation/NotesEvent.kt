c0b9ee08fcd91421e82aa60cf6ecbaea52e6c3c0
SubarnaGhosh <subarnaghosh329@gmail.com>
2024-09-09 13:09:08 +0530

    f

---
+++ app/src/main/java/com/example/briefly/presentation/NotesEvent.kt
@@ -0,0 +1,15 @@
+package com.ahmedapps.roomdatabase.presentation
+
+
+import com.example.briefly.data.Note
+
+sealed interface NotesEvent {
+    object SortNotes: NotesEvent
+
+    data class DeleteNote(val note: Note): NotesEvent
+
+    data class SaveNote(
+        val title: String,
+        val description: String
+    ): NotesEvent
+}
